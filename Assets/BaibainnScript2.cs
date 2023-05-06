d6066be984c307a998c9e74f0968295c6c5f6701
Hana Kataoka <hkataoka925@st1.shinagawajoshi.ed.jp>
2023-05-07 00:31:51 +0900

    わからないいいい

--- Assets/BaibainnScript2.cs
+++ Assets/BaibainnScript2.cs
@@ -33,7 +33,8 @@ public class BaibainnScript2 : MonoBehaviour
         for (int i=0; i<count-count/2; i++)
         {
             yield return new WaitForSeconds(0.1f);
-            Instantiate(this.gameObject,new Vector3(wm.transform.position.x+Random.Range(0.0f, 2.0f),wm.transform.position.y+3.0f,wm.transform.position.z),Quaternion.Euler(90, 90, 0));
+        //     Instantiate(this.gameObject,new Vector3(wm.transform.position.x+Random.Range(0.0f, 2.0f),wm.transform.position.y+3.0f,wm.transform.position.z),Quaternion.Euler(90, 90, 0));
+          Instantiate(this.gameObject,this.transform.position,Quaternion.Euler(90, 90, 0));
         }
      }
 }
